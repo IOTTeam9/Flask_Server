16fecaad96318f9fc17c70c44bbf77e9d25c3a20
jmin314 <jmin314@naver.com>
2023-06-15 01:10:52 +0900

    Update flaskServer.py
    asdlfkjsaldfklasd

--- flaskServer.py
+++ flaskServer.py
@@ -1,18 +1,19 @@
 import pandas as pd
-import numpy as np
 from flask import Flask, jsonify, request
-from apscheduler.schedulers.background import BackgroundScheduler
+from flask_mysqldb import MySQL
 from sklearn.metrics.pairwise import euclidean_distances
-from sklearn.preprocessing import LabelEncoder, StandardScaler
 from collections import Counter
 
 app = Flask(__name__)
 
-scheduler = BackgroundScheduler()
-label_encoder = LabelEncoder()
-std_scaler = StandardScaler()
+app.config['MYSQL_HOST'] = '3.34.148.235'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Iot1234!'
+app.config['MYSQL_DB'] = 'Iot'
+app.config['MYSQL_DATABASE_CONNECT'] = True
 
-df = pd.read_csv('/Users/jangminseong/Downloads/data (2).csv')
+mysql = MySQL(app)
 
 bssid_list = []
 rssi_list = []
@@ -25,13 +26,10 @@ def home():
 
 @app.route('/flask', methods = ['GET', 'POST'])
 def estimate_location():
-    print("민성아힘내")
     global df_POST
     
     if request.method == 'POST':
         data = request.get_json()
-        print("Data get successfully ! ")
-        
         
         bssid_list = []
         rssi_list = []
@@ -42,11 +40,7 @@ def estimate_location():
         df_POST = pd.DataFrame({
                                 'bssid' : list(bssid_list),
                                 'rssi' : list(rssi_list)})
-        
-        # 안드로이드 rssi 읽은값 데이터프레임으로 출력
-        print('Input from android')
-        print(df_POST)
-        
+
         location = "Data Frame"
 
         return jsonify({"location" : location})
@@ -55,9 +49,17 @@ def estimate_location():
         if df_POST.empty:
             return jsonify({'error' : 'No Data'})
         
+        conn = mysql.connection
+        cursor = conn.cursor()
+        query = "SELECT rssi, bssid, place FROM wifi WHERE bssid IN ('{}')".format("', '".join(df_POST['bssid']))
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+        df_wifi = pd.DataFrame(result, columns=['rssi', 'bssid', 'place'])
+        
         place_records = []
         for bssid in df_POST['bssid']:
-            df_matched = df[df['bssid'] == bssid]
+            df_matched = df_wifi[df_wifi['bssid'] == bssid]
             rssi = df_POST[df_POST['bssid'] == bssid]['rssi'].values[0]
             distances = euclidean_distances([[rssi]], df_matched[['rssi']])
             closest_index = distances.argmin()
@@ -69,24 +71,6 @@ def estimate_location():
         most_common = counter.most_common(1)
         location = most_common[0][0]
         
-        # if df_POST.empty:
-        #     return jsonify({'error' : 'No Data'})
-        # df_wifi = df[df['bssid'].isin(df_POST['bssid'])]
-    
-        # print("mySQL Data read successfully")
-        # print(df_wifi)
-
-        # df_POST['bssid'] = [int(x.replace(":", "")[-12:], 16) for x in df_POST['bssid']]
-        # df_wifi['bssid'] = [int(x.replace(":", "")[-12:], 16) for x in df_wifi['bssid']]
-
-        # distances = euclidean_distances(df_POST[['rssi', 'bssid']], df_wifi[['rssi', 'bssid']])
-
-        # closest_index = np.argmin(distances)
-        # closest_data = df_wifi.iloc[closest_index]
-        
-        print("LOCATION")
-        print(location)
-
         if closest_data is not None:
             print("Successfully get location")
             return jsonify({"location" : location})
