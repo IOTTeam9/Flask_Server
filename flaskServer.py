49ecdfe266a2df1bd5d404e0baa61293a2c81112
jmin314 <jmin314@naver.com>
2023-06-14 18:17:59 +0900

    Update flaskServer.py
    ..

--- flaskServer.py
+++ flaskServer.py
@@ -20,19 +20,19 @@ scheduler = BackgroundScheduler()
 label_encoder = LabelEncoder()
 std_scaler = StandardScaler()
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/flask', methods = ['GET', 'POST'])
 def estimate_location():
-    
+
     bssid_list = []
     rssi_list = []
-    
+
     data = request.get_json()
-    
+
     for i in data:
         bssid_list.append(i["bssid"])
         rssi_list.append(i["rssi"])
-        
-    
+
+
     df_query = pd.DataFrame({
                              'bssid' : bssid_list,
                              'rssi' : rssi_list})
@@ -41,26 +41,26 @@ def estimate_location():
     #     'rssi' : [-58, -50, -62]
     # }
     # df_query = pd.DataFrame(df_dic)
-    
+
     conn = mysql.connection
     cursor = conn.cursor()
     query = "SELECT rssi, bssid, place FROM wifi WHERE bssid IN ('{}')".format("', '".join(df_query['bssid']))
     cursor.execute(query)
     result = cursor.fetchall()
-    
+
     df_wifi = pd.DataFrame(result, columns=['rssi', 'bssid', 'place'])
-    
+
     # print(df_query)
     # print(df_wifi)
-    
+
     df_query['bssid'] = [int(x.replace(":", "")[-12:], 16) for x in df_query['bssid']]
     df_wifi['bssid'] = [int(x.replace(":", "")[-12:], 16) for x in df_wifi['bssid']]
-    
+
     distances = euclidean_distances(df_query[['rssi', 'bssid']], df_wifi[['rssi', 'bssid']])
-    
+
     closest_index = np.argmin(distances)
     closest_data = df_wifi.iloc[closest_index]
-    
+
     if closest_data is not None:
         location = closest_data['place']
         return jsonify({location})
@@ -68,4 +68,4 @@ def estimate_location():
         return jsonify({'NaN'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port='5000', debug=True)
